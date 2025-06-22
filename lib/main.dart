36f46b57d20eade031518ce55213b6843d858726
Allan Castro <allancastrou2025@gmail.com>
2025-06-22 15:05:26 -0600

    creacion del widget principal

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Prueba 1',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Material App Bar')),
-        body: const Center(child: Text('Esto es una prueba')),
+        appBar: AppBar(backgroundColor: Colors.green),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            spacing: 40,
+            children: [
+              SizedBox(width: 30),
+              Text('Welcome to Reminders', style: TextStyle(fontSize: 24)),
+
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.event_note_rounded, size: 70, color: Colors.green),
+                  SizedBox(width: 8, height: 25),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Quick Creation',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'simply type in your list, ask siri, or add a reminder from your calendar app',
+                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
